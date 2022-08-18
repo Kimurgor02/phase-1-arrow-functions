c484815feeaf820f7000adff40d88231e4b2590d
Allan <allan.kimurgor@student.moringaschool.com>
2022-08-18 20:03:36 +0300

    Phase 1 arrow functions

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (par1 = 2000, par2 = 100) => par1 / par2;
+divide(2000, 100); 
+const square = (num1 = 2,) => num1**2;
+square (2);
+const add = (numb1 = 1,numb2 = 2) => numb1 + numb2;
+add(1,2);
